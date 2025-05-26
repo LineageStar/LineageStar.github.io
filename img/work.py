4868e3a359da302d17ce1c9932e666ab723bffda
LineageStar <eysikinge@gmail.com>
2025-05-26 09:32:49 +0800

    Site updated: 2025-05-26 09:32:47

--- img/work.py
+++ img/work.py
@@ -2,11 +2,11 @@ import os
 import shutil
 
 current_directory = os.getcwd()
-output_directory = 'G:\\Blog\\themes\\butterfly\\source\\img'
+output_directory = 'E:\\Blog\\themes\\butterfly\\source\\img'
 
 os.makedirs(output_directory, exist_ok=True)
-files_to_rename = [f for f in os.listdir(current_directory) if f.startswith('微') and os.path.isfile(f)]
-for index, filename in enumerate(files_to_rename, start=1):
+files_to_rename = [f for f in os.listdir(current_directory) if f.endswith('master1200') and os.path.isfile(f)]
+for index, filename in enumerate(files_to_rename, start=42):
     new_name = f"{index}.jpg"
     old_path = os.path.join(current_directory, filename)
     new_path = os.path.join(output_directory, new_name)
